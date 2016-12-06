e4290e84a8109d260adecf0556bc717b13ec2849
Ryan Westlake <ryancwestlake@gmail.com>
2016-12-06 15:49:16 -0700

    Install Phantom, first test passes

--- app/routes/reminders.js
+++ app/routes/reminders.js
@@ -2,7 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model() {
-    console.log(this.get('store').findAll('reminder'))
-    return this.get('store').findAll('reminder')
+    console.log(this.get('store').findAll('reminder'));
+    return this.get('store').findAll('reminder');
   }
 });
