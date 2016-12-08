87433158879962f484d76d2795dcdbbf577c7e75
Ryan Westlake <ryancwestlake@gmail.com>
2016-12-08 15:47:54 -0700

    Transform date into real date

--- app/components/new-reminder.js
+++ app/components/new-reminder.js
@@ -13,6 +13,7 @@ export default Ember.Component.extend({
   actions: {
     createReminder() {
       const reminder = this.getProperties('title', 'date', 'notes');
+      reminder.date = new Date(reminder.date)
       this.get('store').createRecord('reminder', reminder).save().then(() => {
         this.setProperties({ title: '', date: '', notes: '' });
       });
