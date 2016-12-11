bd7d851c37bd8172b4f1e4186657f29acd3c6217
Hilary Lewis <hilarylewis92@gmail.com>
2016-12-11 11:07:27 -0700

    Delete welcome route and template

--- app/router.js
+++ app/router.js
@@ -7,7 +7,6 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
-  this.route('welcome');
   this.route('reminders', function() {
     this.route('reminder', { path: ':id'});
     this.route('new');
