777289196da414dfc98534e73dab270ac7cde7c0
Hilary Lewis <hilarylewis92@gmail.com>
2016-12-12 17:35:10 -0700

    Change class name to button instead of link

--- tests/acceptance/new-reminder-test.js
+++ tests/acceptance/new-reminder-test.js
@@ -1,4 +1,4 @@
-import { test } from 'qunit';
+import { test, skip } from 'qunit';
 import moduleForAcceptance from 'remember/tests/helpers/module-for-acceptance';
 
 moduleForAcceptance('Acceptance | new reminder');
@@ -36,3 +36,47 @@ test('add new reminder on click', function(assert) {
     assert.equal(find('.spec-reminder-item').length, 6, 'length of items on page after added reminder');
   });
 });
+
+skip('edit reminder', function(assert) {
+  visit('/');
+
+  click('.spec-link-new-reminder');
+
+  fillIn('.spec-title-field', 'Banana')
+  fillIn('.spec-date-field', '12/08/2016')
+  fillIn('.spec-notes-field', 'Only extra green ones')
+
+  click('.spec-add-reminder')
+
+  click('.spec-reminder-item')
+
+  click('.spec-link-edit-reminder')
+
+  fillIn('.spec-title-field', 'New reminder')
+  fillIn('.spec-date-field', '01/10/2030')
+  fillIn('.spec-notes-field', 'Whats up!')
+
+  click('.spec-add-reminder')
+
+  andThen(function() {
+    assert.equal(find('.spec-selected-reminder').text().trim(), find('.spec-selected-reminder').text().trim(), 'selected reminder text should match list');
+  })
+})
+
+skip('edit reminder button', function(assert) {
+  visit('/');
+
+  click('.spec-link-new-reminder');
+
+  fillIn('.spec-title-field', 'Banana')
+  fillIn('.spec-date-field', '12/08/2016')
+  fillIn('.spec-notes-field', 'Only extra green ones')
+
+  click('.spec-add-reminder')
+
+  click('.spec-reminder-item')
+
+  andThen(function() {
+    assert.equal(find('.spec-link-edit-reminder').length, 1, 'should have an edit button');
+  })
+})
